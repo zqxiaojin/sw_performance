e58bcbe05243fd1214cbd3e9dd4fb2212f10a7ab
lj98586 <lj98586@alibaba-inc.com>
2017-09-12 19:07:51 +0800

    兼容log

--- src/fetchspeed/sw.js
+++ src/fetchspeed/sw.js
@@ -7,15 +7,34 @@ self.addEventListener('fetch', function(event) {
 	// console.log(".respondWith", event.respondWith);
 	// console.log(".default", event.default);
 
-	if (event.request.url.indexOf("bigJson.js") != -1) {
+	if (event.request.url.indexOf("jsonBig.json") != -1) {
 
 
 		if (event.request.url.indexOf("fake") != -1) {
 
-			event.respondWith(fetch(event.request));
+			event.respondWith(new Response("Hello world!"));
 		} else {
 			event.respondWith(fetch(event.request));			
 		}
 
 	}
 });
+
+
+
+var PRECACHE = "fake";
+
+
+const PRECACHE_URLS = [
+  'jsonBig.json',
+];
+
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
